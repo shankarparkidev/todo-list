300d87baa0ef395355b284d1952099827b0b0b68
Shankar Parki <shankar.parki.dev@gmail.com>
2026-09-04 23:46:02 +0530

    chore: initialize todo list project

---
+++ script.js
@@ -0,0 +1,5 @@
+const todoInput = document.getElementById("todoInput");
+const addTodoButton = document.getElementById("addTodoButton");
+const todoList = document.getElementById("todoList");
+
+console.log("Todo List application loaded.");
